58b577b1bb297935745a498939af3103a5b9b34d
Achim Krauch <achim.krauch@gmail.com>
2017-09-08 12:35:39 +0200

    Add support for point type XYZRGBNormal.

---
+++ src/point_xyzrgbnormal.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+ *  Copyright (c) 2017, Intermodalics BVBA                                    *
+ *  All rights reserved.                                                      *
+ ******************************************************************************/
+
+#include <rtt_pcl/typekit/Types.hpp>
+
+// pcl::PointCloud<pcl::PointXYZRGBNormal>
+template class RTT::internal::DataSource< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::internal::AssignableDataSource< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::internal::AssignCommand< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::internal::ValueDataSource< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::internal::ConstantDataSource< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::internal::ReferenceDataSource< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::OutputPort< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::InputPort< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::Property< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::Attribute< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
+template class RTT::Constant< pcl::PointCloud<pcl::PointXYZRGBNormal> >;
